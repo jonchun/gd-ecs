cb89b0d57c9ac90ff075f324f70dd1a340da1eb3
Jonathan Chun <git@jonathanchun.com>
2020-07-25 07:16:25 -0400

    renamed some components and removed unused properties from C_KinematicMotion2D

--- gd-ecs-project/src/Components/C_KinematicMotion2D.gd
+++ gd-ecs-project/src/Components/C_KinematicMotion2D.gd
@@ -7,8 +7,6 @@ export var acceleration_default := Vector2(5000.0, 0.0)
 export var max_speed_default := Vector2(400.0, 1200.0)
 
 var acceleration: Vector2
-var on_floor: bool
-var on_wall: bool
 var velocity: Vector2
 var max_speed: Vector2
 
